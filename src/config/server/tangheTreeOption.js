7d436b1a168f88352e66bff820393e171bf2c950
jinsheng <461969446@qq.com>
2020-09-21 10:22:45 +0800

    dev

--- src/config/server/tangheTreeOption.js
+++ src/config/server/tangheTreeOption.js
@@ -1,7 +1,7 @@
 
 const SERVER_HOST = "http://172.20.83.223:8098/iserver/services";
-const SW_DATA = "/data-TangHeShuJu/rest/data";
-const SW_DATA_NAME = "all:";
+const SW_DATA = "/data-th_zy/rest/data";
+const SW_DATA_NAME = "th_zy:";
 const SERVER_DEFAULT_DATA = SERVER_HOST + SW_DATA;
 
 //  控规专题
@@ -19,16 +19,8 @@ export const FIXED_SOURCE = [
     label: "项目",
     url: SERVER_DEFAULT_DATA,
     dataset: `${SW_DATA_NAME}项目`,
-    icon: '在建项目',
+    icon: false,
     type: "mvt",
-  },
-  {
-    id: "断点",
-    label: "断点",
-    url: SERVER_DEFAULT_DATA,
-    dataset: `${SW_DATA_NAME}断点`,
-    icon: '卡点',
-    type: "mvt"
   },
 ];
 
